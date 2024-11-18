19c53096212860f52fb6d73ba902d083dbb2e41d
Fadi <fadi@Fadis-MacBook-Pro.local>
2024-11-18 22:58:44 +0100

    change | adjusted reset page view

--- app/[lng]/auth/reset/page.tsx
+++ app/[lng]/auth/reset/page.tsx
@@ -1,9 +1,26 @@
 import React from 'react';
 import ResetForm from "@/components/auth/reset-form";
+import Image from "next/image";
 
 const ResetPage = () => {
     return (
-        <ResetForm/>
+        <div className="flex items-center justify-center">
+            <div className="flex w-screen h-screen">
+                <div className="hidden lg:flex w-1/2 p-2">
+                    <Image
+                        className="rounded-xl"
+                        width={1920}
+                        height={300}
+                        src={`/assets/password-bg.jpg`}
+                        alt="login-bg"
+                    />
+                </div>
+                <div className="flex w-full items-center justify-center lg:w-1/2">
+                    <ResetForm/>
+                </div>
+            </div>
+        </div>
+
     );
 };
 
